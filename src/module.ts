8e6a16a04edb30f9a10ae992d0f3beab288c3d29
Sacha STAFYNIAK <sacha@digisquad.io>
2022-06-24 12:17:17 +0200

    wip: extract typescript props info from AST

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,7 @@ import { readFile } from 'fs/promises'
 import { defineNuxtModule, resolveModule, createResolver, addServerHandler } from '@nuxt/kit'
 import { parse, compileScript, compileTemplate } from '@vue/compiler-sfc'
 import type { SFCDescriptor } from '@vue/compiler-sfc'
+import * as ts from 'typescript'
 
 export interface ModuleOptions {}
 
@@ -20,16 +21,19 @@ export default defineNuxtModule<ModuleOptions>({
           const name = (component as any).pascalName
           const path = resolveModule((component as any).filePath, { paths: nuxt.options.rootDir })
           const source = await readFile(path, { encoding: 'utf-8' })
+          const props = []
 
           // Parse component source
           const { descriptor } = parse(source)
 
-          // Parse script
-          const { props } = descriptor.scriptSetup
-            ? parseSetupScript(name, descriptor)
-            : {
-                props: []
-              }
+          if (descriptor.scriptSetup?.lang === 'ts') {
+            // parse typescript setup script
+            const parsed = parseTSSetupScript(name, descriptor)
+            props.push(...parsed.props)
+          } else if (descriptor.scriptSetup) {
+            const parsed = parseSetupScript(name, descriptor)
+            props.push(...parsed.props)
+          }
 
           const { slots } = parseTemplate(name, descriptor)
 
@@ -102,5 +106,84 @@ function parseTemplate (id: string, descriptor: SFCDescriptor) {
 
   return {
     slots: findSlots(template.ast?.children || [])
+  }
+}
+
+function parseTSSetupScript (id: string, descriptor: SFCDescriptor) {
+  const props = []
+
+  // const program = ts.createProgram([`${id}.vue.ts`], {})
+  const source = ts.createSourceFile(
+    `${id}.vue.ts`,
+    descriptor.scriptSetup.content,
+    ts.ScriptTarget.Latest,
+    true
+  )
+
+  // 1. extract file infos (types, extrernal types and defineProps / defineEmits calls)
+  ts.forEachChild(source, (node) => {
+    if (ts.isImportDeclaration(node)) {
+      // extract imported types
+      const path = node.moduleSpecifier.getText()
+      let imports = []
+
+      if (node.importClause.isTypeOnly && node.importClause.namedBindings.kind === ts.SyntaxKind.NamedImports) {
+        imports = node.importClause.namedBindings.elements.map(e => e.name.text)
+      }
+
+      console.log({
+        path,
+        imports
+      })
+    } else if (ts.isTypeAliasDeclaration(node)) {
+      // extract type aliases
+      const name = node.name.text
+      const type = node.type.getText()
+
+      console.log({
+        name,
+        type
+      })
+    } else if (ts.isInterfaceDeclaration(node)) {
+      // extract interfaces
+
+      const name = node.name.escapedText
+      const members = node.members.map((m, index) => {
+        if (ts.isCallSignatureDeclaration(m)) {
+          // call signatures should be emits declarations
+          // extract call parameters
+          console.log(m.typeParameters)
+          return {
+            key: index,
+            type: m.getText()
+          }
+        } else if (ts.isPropertySignature(m)) {
+          // members should be properties
+          return {
+            key: m.name.getText(),
+            type: m.type.getText()
+          }
+        }
+
+        return {
+          key: 'unknown',
+          type: 'unknown'
+        }
+      })
+      console.log({
+        name,
+        members
+      })
+    } else if (ts.isVariableStatement(node)) {
+      // here we need to find defineEmits / withDefaults / defineProps calls
+      // and extract either the generic type info or the default value of destructured props
+      console.log(node.kind)
+    }
+  })
+
+  // 2. resolve defineProps / defineEmits type parameters to the actual types
+
+  return {
+    props
   }
 }
