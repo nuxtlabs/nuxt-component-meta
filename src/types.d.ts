f3cdfe769abffe76c774b9cf78eb0291067b3404
Ahad Birang <farnabaz@gmail.com>
2022-07-15 15:37:11 +0200

    feat: basic support for typescript defineProps

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,10 @@
+export interface ComponentPropType {
+  type: string
+}
+
 export interface ComponentProp {
     name: string
-    type?: string,
+    type?: string | ComponentPropType,
     default?: any
     required?: boolean,
     values?: any,
