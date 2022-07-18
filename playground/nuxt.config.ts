6e70ad3bde35d5d04cbe8eaaf5cdff39b0186cf7
Ahad Birang <farnabaz@gmail.com>
2022-07-18 13:13:32 +0200

    feat: add flag for global components (#27)

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 import nuxtMetaModule from '../src/module'
 
 export default defineNuxtConfig({
+  components: {
+    dirs: [
+      {
+        path: '~/components/global',
+        global: true
+      },
+      '~/components'
+    ]
+  },
   modules: [
     nuxtMetaModule
   ]
